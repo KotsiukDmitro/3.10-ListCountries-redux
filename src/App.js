dec6062612434b31907bf9cbd04ae087f00e4d12
kotsiuk dmitro <kotsiukdmitro@gmail.com>
2025-08-05 10:41:59 +0300

    improve code

--- src/App.js
+++ src/App.js
@@ -1,92 +1,116 @@
-import './App.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggle } from './store/themeSlice';
 import { getCountries } from './store/countrySlice';
 import CountryDetails from './CountryDetails';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
 
+// 🕓 Debounce-хук
+function useDebounce(value, delay) {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounced(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debounced;
+}
 
 function App() {
-  const [country, setCountry] = useState('')
+  const [country, setCountry] = useState('');
+  const [inputRegion, setInputRegion] = useState('');
+  const [openCountryDetails, setOpenCountryDetails] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const debouncedCountry = useDebounce(country, 500);
+
+  const dispatch = useDispatch();
+  const theme = useSelector(state => state.theme.themes);
+  const listCountries = useSelector(state => state.countries.countries);
+
   function searchCountry(event) {
-    setCountry(event.target.value)
+    setCountry(event.target.value);
   }
-  const [inputRegion, setInputRegion] = useState('')
+
   function handleChangeRegion(event) {
-    setInputRegion(event.target.value)
+    setInputRegion(event.target.value || '');
   }
 
-  const dispatch = useDispatch()
-  const theme = useSelector(state => state.theme.themes)
   function changeTheme() {
-    dispatch(toggle())
+    dispatch(toggle());
   }
 
-  const listCountries = useSelector(state => state.countries.countries)
-
-  const [openCountryDetails, setOpenCountryDelails] = useState()
   function openClose() {
-    setOpenCountryDelails((prevState => !prevState))
+    setOpenCountryDetails(prev => !prev);
   }
 
-  const [selectedItem, setSelectedItem] = useState()
   function handleItem(item) {
-    setSelectedItem(item)
-    openClose()
+    setSelectedItem(item);
+    openClose();
   }
 
   useEffect(() => {
-    getCountries(dispatch, country, inputRegion)
-  }, [dispatch, country, inputRegion])
+    getCountries(dispatch, debouncedCountry, inputRegion);
+  }, [dispatch, debouncedCountry, inputRegion]);
 
-  return (<>
-    <CountryDetails open={openCountryDetails} openClose={openClose} element={selectedItem} />
-
-    <div className={theme}>
-      <div className='container'>
-        <div className='header'>
-          <h3 className='header-text'>Where in the world?</h3>
-          <div>
-            <FontAwesomeIcon icon={faLightbulb} />
-            {/* <i class="fa-solid fa-lightbulb-on"></i> */}
-            <button className='btn-theme' onClick={changeTheme}>Light/Dark</button>
+  return (
+    <>
+      <CountryDetails open={openCountryDetails} openClose={openClose} element={selectedItem} />
+      <div className={theme}>
+        <div className='container'>
+          <div className='header'>
+            <h3 className='header-text'>Where in the world?</h3>
+            <div>
+              <FontAwesomeIcon icon={faLightbulb} />
+              <button className='btn-theme' onClick={changeTheme}>Light/Dark</button>
+            </div>
           </div>
-        </div>
-        <div className='row-search'>
-          <input className='input-search-country' type='text' placeholder='Search of a country' value={country} onChange={searchCountry}></input>
-          <select className='select-region' value={inputRegion} onChange={handleChangeRegion}>
-            <option value=''>All regions</option>
-            <option value='Africa'>Africa</option>
-            <option value='Americas'>Americas</option>
-            <option value='Asia'>Asia</option>
-            <option value='Europe'>Europe</option>
-            <option value='Oceania'>Oceania</option>
-          </select>
-        </div>
-        <div className='selected-countries'>
-          {listCountries.map(item => {
-            return (
+          <div className='row-search'>
+            <input
+              className='input-search-country'
+              type='text'
+              placeholder='Search for a country'
+              value={country}
+              onChange={searchCountry}
+            />
+            <select className='select-region' value={inputRegion} onChange={handleChangeRegion}>
+              <option value=''>All regions</option>
+              <option value='Africa'>Africa</option>
+              <option value='Americas'>Americas</option>
+              <option value='Asia'>Asia</option>
+              <option value='Europe'>Europe</option>
+              <option value='Oceania'>Oceania</option>
+            </select>
+          </div>
+          <div className='selected-countries'>
+            {listCountries.map(item => (
               <div className='card' key={item.cca3} onClick={() => handleItem(item)}>
                 <img alt='flag' className='flag' src={item.flags.svg} />
                 <div>
                   <h3 className='name-country'>{item.name.common}</h3>
-                  <h5 className='country-info'>Population: <span className='data'>{item.population.toLocaleString()}</span></h5>
-                  <h5 className='country-info'>Region: <span className='data'>{item.region}</span></h5>
-                  <h5 className='country-info'>Capital: <span className='data'>{item.capital}</span></h5>
+                  <h5 className='country-info'>
+                    Population: <span className='data'>{item.population.toLocaleString()}</span>
+                  </h5>
+                  <h5 className='country-info'>
+                    Region: <span className='data'>{item.region}</span>
+                  </h5>
+                  <h5 className='country-info'>
+                    Capital: <span className='data'>{item.capital?.[0]}</span>
+                  </h5>
                 </div>
               </div>
-            )
-
-          })}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-
-  </>
-
+    </>
   );
-
 }
+
 export default App;
--- src/store/countrySlice.js
+++ src/store/countrySlice.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from 'axios';
+
 const countrySlice = createSlice({
     name: 'country',
     initialState: {
@@ -7,41 +8,43 @@ const countrySlice = createSlice({
     },
     reducers: {
         setCountries(state, actions) {
-            state.countries = actions.payload
+            state.countries = actions.payload;
         }
     }
-})
+});
+
 export const getCountries = async (dispatch, country, region) => {
-    const urlMain = 'https://restcountries.com/v3.1'
-    const url = country ? `${urlMain}/name/${country}` : `${urlMain}/all`
-    axios({
-        method: 'get',
-        url: url
-    })
-        .then(function (response) {
-            const list = changeCountriesList(response, region)
-            dispatch(setCountries(list))
-            console.log(response);
-        })
-}
+    const urlMain = 'https://restcountries.com/v3.1';
+    const isValidCountry = country && country.trim() !== '';
+    const fields = 'name,region,flags,population,capital,cca3';
+    const url = isValidCountry
+        ? `${urlMain}/name/${encodeURIComponent(country.trim())}?fields=${fields}`
+        : `${urlMain}/all?fields=${fields}`;
+
+    try {
+        const response = await axios.get(url);
+        const list = changeCountriesList(response, region);
+        dispatch(setCountries(list));
+        console.log(response);
+    } catch (error) {
+        console.error('Ошибка при получении стран:', error.response?.data || error.message);
+        dispatch(setCountries([])); // Очистка списка при ошибке
+    }
+};
+
 function sortCountry(i, j) {
-    if (i.name.common < j.name.common) {
-        return -1
-    }
-    if (i.name.common > j.name.common) {
-        return 1
-    }
-    return 0
+    if (i.name.common < j.name.common) return -1;
+    if (i.name.common > j.name.common) return 1;
+    return 0;
 }
+
 function changeCountriesList(response, inputRegion) {
     const country = response.data.filter(item => {
-        if (inputRegion === '') {
-            return true
-        }
-        if (inputRegion === item.region)
-            return true
-    })
-    return country.sort(sortCountry)
+        if (inputRegion === '') return true;
+        return inputRegion === item.region;
+    });
+    return country.sort(sortCountry);
 }
-export const { setCountries } = countrySlice.actions
-export default countrySlice.reducer
+
+export const { setCountries } = countrySlice.actions;
+export default countrySlice.reducer;
